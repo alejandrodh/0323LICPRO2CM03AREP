f130a3c010a4c40ebe6ae3a0d8b241e6856d805e
alejandrodh <alejandro@digitalhouse.com>
2023-05-17 14:06:55 -0300

    HTTP get, post y hashing.

--- clase16_httpYHashing/controllers/indexController.js
+++ clase16_httpYHashing/controllers/indexController.js
@@ -4,7 +4,32 @@ let op = db.Sequelize.Op;
 let indexController = {
     index: function(req, res){
         return res.render('index'); 
+    },
+    search: function(req, res){
+        let aBuscar = req.query.buscar;
+        //Usar un método de sequelize para obtener los datos.
+
+            //Dentro del then, renderizar la vista y mostrar la información.
+
+        return res.send(aBuscar);
+    },
+    register: function(req, res){
+        return res.render('register');
+    },
+    store: function(req, res){
+        let form = req.body
+
+        //Encriptar la contraseña antes de guardar en la base de datos.
+
+        //Usar un método de Sequelize para guardar datos.
+
+            //Dentro del then debería redireccionar a otra ruta.
+
+        // return res.send(form);
+        return res.redirect('/movies');
+
     }
+
 }
 
 module.exports = indexController
--- clase16_httpYHashing/routes/index.js
+++ clase16_httpYHashing/routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 let indexController = require("../controllers/indexController")
 
 /* GET home page. */
-router.get('/', indexController.index)
+router.get('/', indexController.index);
+router.get('/search-results', indexController.search);
+router.get('/register',indexController.register); //Mostrar el form de registro.
+
+router.post('/register', indexController.store); //Guarda al usuario en la base de datos.
 
 module.exports = router;
