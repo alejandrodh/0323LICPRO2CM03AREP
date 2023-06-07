9440d0e2b274f863d59b1ff58f426df0e010ae0d
alejandrodh <alejandro@digitalhouse.com>
2023-06-07 05:18:29 -0300

    test deploy en Vercel

--- clase21_deploy/database/config/config.js
+++ clase21_deploy/database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": 271323,
+    "password": "udesamovies",
+    "database": "deployprog2_moviesdb", //Tiene que ser el nombre de la base de datos en tu motor de base de datos. Lo estas viendo en Workbench.
+    "host": "mysql-deployprog2.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": 271323,
+    "password": "udesamovies",
+    "database": "deployprog2_moviesdb", //Tiene que ser el nombre de la base de datos en tu motor de base de datos. Lo estas viendo en Workbench.
+    "host": "mysql-deployprog2.alwaysdata.net",
     "dialect": "mysql"
   }
 }
